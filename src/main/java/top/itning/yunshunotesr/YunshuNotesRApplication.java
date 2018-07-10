4ebd671da96c09955d1190c3190dd28bf39bacc9
itning <itning@itning.top>
2018-07-10 15:03:10 +0800

    init

---
+++ src/main/java/top/itning/yunshunotesr/YunshuNotesRApplication.java
@@ -0,0 +1,18 @@
+package top.itning.yunshunotesr;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+
+/**
+ * Spring Boot 启动类
+ *
+ * @author itning
+ */
+@SpringBootApplication
+public class YunshuNotesRApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(YunshuNotesRApplication.class, args);
+    }
+}
